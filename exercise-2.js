833e384f6cc3e5d5b54f21aa07cbb42c857fd06c
saveroz <muhammadsavero@gmail.com>
2019-05-27 11:40:37 +0700

    fix some trivial mistake

--- exercise-2.js
+++ exercise-2.js
@@ -1,9 +1,9 @@
 function balikString(kata) {
     // you can only write your code here!
     var result='';
-    for (let char of kata){
-        result=char+result;
+    for (var i=kata.length-1;i>=0;i--){
+        result+=kata[i];
     }
     return result;
   }
-  
+  console.log(balikString('hello'));
